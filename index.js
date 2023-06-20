f15b9376b09dc0114c1a3455721447f1b1a08f6c
philippe_mertens <pme@taktik.com>
2023-06-20 13:41:44 +0200

    Test

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-const Keyboard = window.SimpleKeyboard.default;
+// const Keyboard = window.SimpleKeyboard.default;
 
-const myKeyboard = new Keyboard({
-  onChange: input => onChange(input),
-  onKeyPress: button => onKeyPress(button)
-});
+// const myKeyboard = new Keyboard({
+//   onChange: input => onChange(input),
+//   onKeyPress: button => onKeyPress(button)
+// });
 
-const keyboard = new Keyboard({
-  onChange: input => onChange(input),
-  onKeyPress: button => onKeyPress(button)
-});
+// const keyboard = new Keyboard({
+//   onChange: input => onChange(input),
+//   onKeyPress: button => onKeyPress(button)
+// });
 
-function onChange(input){
-  document.querySelector("input").value = input;
-  console.log("Input changed", input);
-}
+// function onChange(input){
+//   document.querySelector("input").value = input;
+//   console.log("Input changed", input);
+// }
 
-function onKeyPress(button){
-  console.log("Button pressed", button);
-}
+// function onKeyPress(button){
+//   console.log("Button pressed", button);
+// }
+//import KioskBoard from 'kioskboard';
+
+const KioskBoard = window.KioskBoard
+KioskBoard.run('.js-kioskboard-input', {})

9de15dcdf5b611517356f886133758ef1f005faa
philippe_mertens <pme@taktik.com>
2023-06-26 22:38:35 +0200

    Test

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
 function injectHeadJS() {
-
-  var frames = window.frames; // ou // var frames = window.parent.frames;
-for (var i = 0; i < frames.length; i++) {
-  // faire quelque chose avec chaque subframe en tant que frames[i]
-  console.log(frames[i])
-}
-
-  var iFrameHead = window.frames["iframe-1"].document.getElementsByTagName("head")[0];
+  var iFrameHead = document.getElementById["iframe-1"].contentWindow.document.getElementsByTagName("head")[0];
   var kbScript = document.createElement("script");
   kbScript.type = "text/javascript";
   kbScript.src = "dist/kioskboard-aio-2.3.0.min.js";
@@ -16,7 +9,7 @@ for (var i = 0; i < frames.length; i++) {
 }
 
 function injectBodyJS() {
-  var iFrameHead = window.frames["iframe-1"].document.getElementsByTagName("body")[0];
+  var iFrameHead = window.frames["iframe-1"].contentWindow.document.getElementsByTagName("body")[0];
   var kbScript = document.createElement("script");
   kbScript.type = "text/javascript";
   kbScript.src = "iframe-kb-setup.js";

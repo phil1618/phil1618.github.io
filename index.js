bcb00767c7fb507b34999ae4d4876a43eb1eb33b
philippe_mertens <pme@taktik.com>
2023-06-26 22:08:59 +0200

    Clean up

--- index.js
+++ index.js
@@ -1,27 +1,5 @@
-// const Keyboard = window.SimpleKeyboard.default;
-
-// const myKeyboard = new Keyboard({
-//   onChange: input => onChange(input),
-//   onKeyPress: button => onKeyPress(button)
-// });
-
-// const keyboard = new Keyboard({
-//   onChange: input => onChange(input),
-//   onKeyPress: button => onKeyPress(button)
-// });
-
-// function onChange(input){
-//   document.querySelector("input").value = input;
-//   console.log("Input changed", input);
-// }
-
-// function onKeyPress(button){
-//   console.log("Button pressed", button);
-// }
-//import KioskBoard from 'kioskboard';
-
 const KioskBoard = window.KioskBoard
-//KioskBoard.run('.js-kioskboard-input', {})
+
 KioskBoard.init({
 
   /*!
@@ -109,4 +87,5 @@ KioskBoard.init({
   // The Enter key can close and remove the keyboard. Prevented when "false"
   keysEnterCanClose: true,
 });
+
 KioskBoard.run('.js-kioskboard-input')

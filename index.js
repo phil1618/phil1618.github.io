11e07080410793bb44a03760fb34eb7e52872be3
philippe_mertens <pme@taktik.com>
2023-12-05 09:14:38 +0100

    Test

--- index.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {
-    // Display the current URL in the <p> element with id "urlDisplay"
-    var urlDisplayElement = document.getElementById('urlDisplay');
-    urlDisplayElement.textContent = window.location.href;
-
-    // Your additional JavaScript code can go here
-    // For example:
-    // alert("Hello from the external script!");
-};
